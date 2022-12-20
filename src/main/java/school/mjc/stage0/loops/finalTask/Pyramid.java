4e79820cd6db67e87ccbb716e9f089c85d2fab74
Nikolay-zhukau <koliajukov@gmail.com>
2022-12-20 13:48:34 +0300

    made some cosmetic changes

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -3,20 +3,20 @@ package school.mjc.stage0.loops.finalTask;
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
         for (int row = 0; row < cathetusLength; row++) {
-            int printedNumber = row + 1;
-            int spaceCount = cathetusLength - printedNumber;
-            for (int spaceIn = 0; spaceIn < spaceCount; spaceIn++) {
+            int numberOfPrintedNumbers = row + 1;
+            int spaceCount = cathetusLength - numberOfPrintedNumbers;
+            for (int printSpace = 0; printSpace < spaceCount; printSpace++) {
                 System.out.print(" ");
             }
-            for (int col = 0; col < printedNumber * 2 + 1; col++) {
-                int numByCol = printedNumber - col;
-                if (numByCol == 0 || numByCol == -1) {
+            for (int col = 0; col < numberOfPrintedNumbers * 2 + 1; col++) {
+                int printedNumbers = numberOfPrintedNumbers - col;
+                if (printedNumbers == 0 || printedNumbers == -1) {
                     continue;
                 }
-                if (numByCol < 0) {
-                    numByCol = numByCol * -1;
+                if (printedNumbers < 0) {
+                    printedNumbers = printedNumbers * -1;
                 }
-                System.out.print(numByCol);
+                System.out.print(printedNumbers);
             }
 
 
